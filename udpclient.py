7ffc7ce78e477e0c90394c3b27c6fd8e8b396335
Fortune <lachesis.f@gmail.com>
2015-03-29 19:54:17 +0800

    modified:   udpclient.py

--- udpclient.py
+++ udpclient.py
@@ -8,7 +8,7 @@ import binascii
 args = sys.argv;
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-data = args[1]
+data = raw_input("hex:")
 print 'send data',data
 hexdata = binascii.a2b_hex(data)
 s.sendto(hexdata,('127.0.0.1',9999))
