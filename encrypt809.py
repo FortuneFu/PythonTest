e957bd8134ab4c9b3ac7a058f68bdbb041b85564
Fortune <lachesis.f@gmail.com>
2015-04-02 23:45:10 +0800

    test

--- encrypt809.py
+++ encrypt809.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import binascii
@@ -8,21 +8,21 @@ IA1 = 20000000
 IC1 = 30000000
 
 
-def encrypt(key, buffer):
+def encrypt(key_pram, buffer_pram):
     idx = 0
-    if (key == 0):
-        key = 1
+    if key_pram == 0:
+        key_pram = 1
 
-    while (idx < len(buffer)):
-        key = IA1 * (key % M1) + IC1
+    while idx < len(buffer_pram):
+        key_pram = IA1 * (key_pram % M1) + IC1
         # 模拟C的无符号int溢出
-        key = key % (2 ** 32)
-        # print(key)
-        buffer[idx] ^= ((key >> 20) & 0xFF)
+        key_pram %= 2 ** 32
+        # print(key_pram)
+        buffer_pram[idx] ^= ((key_pram >> 20) & 0xFF)
         idx += 1
 
 
-def removeXOR(ba):
+def remove_xor(ba):
     # ba = bytearray(binascii.a2b_hex(hex_org))
     ba_new = ba.replace(b'\x5a\x01', b'\x5b').replace(b'\x5a\x02', b'\x5a').replace(b'\x5e\x01', b'\x5d').replace(
         b'\x5e\x02', b'\x5e')
@@ -30,10 +30,10 @@ def removeXOR(ba):
 
 
 if __name__ == '__main__':
-    key = input("key:")
-    hex_str = input("hex_str:")
+    key = raw_input("key:")
+    hex_str = raw_input("hex_str:")
     buffer = bytearray(binascii.a2b_hex(hex_str))
-    buffer = removeXOR(buffer)
-    print("rXOR:", binascii.b2a_hex(buffer))
+    buffer = remove_xor(buffer)
+    print "rXOR:", binascii.b2a_hex(buffer)
     encrypt(int(key, 16), buffer)
-    print("result:", binascii.b2a_hex(buffer))
+    print "result:", binascii.b2a_hex(buffer)
--- removeXOR.py
+++ removeXOR.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import binascii
 
 hex_org = "5a01fffe5a020203"
 ba = bytearray(binascii.a2b_hex(hex_org))
-print(hex_org)
-ba_new = ba.replace(b'\x5a\x01',b'\x5b').replace(b'\x5a\x02',b'\x5a').replace(b'\x5e\x01',b'\x5d').replace(b'\x5e\x02',b'\x5e')
-print(binascii.b2a_hex(ba_new))
+print hex_org
+ba_new = ba.replace(b'\x5a\x01', b'\x5b').replace(b'\x5a\x02', b'\x5a').replace(b'\x5e\x01', b'\x5d').replace(
+    b'\x5e\x02', b'\x5e')
+print binascii.b2a_hex(ba_new)
